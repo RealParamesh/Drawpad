10fe0173c82cd8c3e69aca21498c6fa7215e2e65
Paramesh S <71922456+RealParamesh@users.noreply.github.com>
2021-06-29 23:02:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,6 +5,9 @@ var bslide;
 var cbutton;
 var dbutton;
 var state="nod";
+var ms;
+var ssr;
+
 function setup() {
   createCanvas(windowWidth-10,windowHeight-65);
   sslide= createSlider(0,200,0);
@@ -13,8 +16,9 @@ function setup() {
   bslide= createSlider(0,255,0);
   cbutton= createButton("clear");
   dbutton= createButton("download your pic");
+  ms=createInput("circle");
   background("black");
-  createP("(size rgb ) and r g b won't be captured in pic it will appear the fill selected & my brand name(not selling anything) , for comments message me at parameshgaming30@gmail.com")
+ //createP("(size rgb ) and r g b won't be captured in pic it will appear the fill selected & my brand name(not selling anything) , for comments message me at parameshgaming30@gmail.com")
 }
 
 function draw() {
@@ -23,12 +27,21 @@ function draw() {
   fill("white");
   if(mouseIsPressed){
     fill(rslide.value(),gslide.value(),bslide.value());
+    if(ms.value()=="circle"){
    ellipse(mouseX,mouseY,sslide.value(),sslide.value()); 
+    }
+    else if(ms.value()=="square"){
+      rect(mouseX,mouseY,sslide.value(),sslide.value());
+    }
+    else if(ms.value()=="triangle"){
+      triangle(mouseX-100, mouseY, mouseX+100, mouseY, mouseX, mouseY-150)
+    }
     rect(0,0,100,100);
   } 
   if(mouseIsPressed){
     state="d";
   }
+  
   if(state=="nod"){
      text(rslide.value(),30,30);  
      text(gslide.value(),30,50); 
@@ -38,13 +51,19 @@ function draw() {
      text("b",10,70); 
      text("size,rgb",20,height-5);
 }
+ssr=sslide.value()
   cbutton.mousePressed(cllear);
   dbutton.mousePressed(download);
 }
 function cllear(){
   fill('black');
+  sslide.value(0);
+  rslide.value(0);
+  gslide.value(0);
+  bslide.value(0);
  ellipse(200,200,10000,10000);
-  
+//  console.log(sslide.value());
+//  mouseReleased(sslide.value(0));
 }
 function download(){
   state="d";
@@ -54,10 +73,11 @@ function download(){
   rect(20,height-20,70,20);
     fill("white");
     textSize(10);
-     text("parameshCodes",5,10)
+     text("parameshCodes",5,10);
   save();
   }
 }
+
 
 function mouseReleased(){
     (state="nod");
